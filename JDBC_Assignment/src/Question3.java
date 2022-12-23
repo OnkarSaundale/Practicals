9197830593a77ace1bd8411cdd2d4c79b6e1792b
Onkar19 <101797547+OnkarSaundale@users.noreply.github.com>
2022-12-23 10:28:37 +0530

    Update Question3.java

--- JDBC_Assignment/src/Question3.java
+++ JDBC_Assignment/src/Question3.java
@@ -1,10 +1,11 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Types;
 import java.util.Scanner;
 import com.mysql.jdbc.CallableStatement;
 //Create procedure AddnewCustomers() and SearchById() in mysql database
 //call above procedure by jdbc's Callable statement..
-//i)
+
 public class Question3 {
 
 	public static void main(String[] args) 
@@ -17,21 +18,33 @@ public class Question3 {
 			System.out.println("Connection established.");
 			
 			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter rollno, name, result");
+	//do one at a time
+	//i)procedure AddnewCustomers()
 			
+		//	System.out.println("Enter rollno, name, result");
+		//	int rn=sc.nextInt();
+		//	String nm=sc.next();
+		//	int m =sc.nextInt();
 			
-			int rn=sc.nextInt();
-			String nm=sc.next();
-			int m =sc.nextInt();
+		//	CallableStatement cs= (CallableStatement) con.prepareCall("{call procstud(?,?,?)}");
 			
-			CallableStatement cs= (CallableStatement) con.prepareCall("{call procstud(?,?,?)}");
+	//		cs.setInt(1, rn);
+	//		cs.setString(2, nm);
+	//		cs.setInt(3, m);
+	//		cs.execute();
+	//		System.out.println("Procedure Executed..");
 			
+	//ii) procedure SearchById()
+			
+			System.out.println("Enter Roll no:");
+			int rn =sc.nextInt(); 
+			
+			CallableStatement cs=(CallableStatement) con.prepareCall("{call SearchById(?,?)}");
 			cs.setInt(1, rn);
-			cs.setString(2, nm);
-			cs.setInt(3, m);
+			cs.registerOutParameter(2, Types.VARCHAR);
 			cs.execute();
-			System.out.println("Procedure Executed..");
-			//ii) second part is remaining...
+			System.out.println("Name of the student:"+" "+cs.getString(2));
+			con.close();
 		}
 		catch(Exception e)
 		{
