196a91328f2fd5781d7f846ba99cfcb97aa5c565
Onkar19 <101797547+OnkarSaundale@users.noreply.github.com>
2022-12-29 10:57:26 +0530

    Add files via upload

--- my_pack/EmpMain.java
+++ my_pack/EmpMain.java
@@ -1,26 +1,39 @@
-package my_pack;
+package Component_Mapping;
+//Has a kind of relationship
 import org.hibernate.Session;
-import org.hibernate.*;
-import org.hibernate.cfg.*;
-
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class EmpMain {
 
 	public static void main(String[] args)
 	{
 		Configuration cfg=new Configuration();
-        cfg.configure("hibernate.cfg.xml");
+        cfg.configure("hibernate_compo_map.cfg.xml");//Configuration done by this file
+        
         SessionFactory factory=cfg.buildSessionFactory();
+        
         Session session=factory.openSession();
+        
         Transaction tr=session.beginTransaction();
-        Employee e1=new Employee();
-        e1.setEmpid(3);
-        e1.setEmpname("Sumit ");
-        e1.setSalary(50000);
+        //Employee has a address
+        Address addr= new Address();//Address class object here.
+        addr.setCity("Pune");
+        addr.setCountry("India");
+        addr.setState("Maharashtra");
+        
+        Employee e1 = new Employee();//Persistance class object here.
+        e1.setEmpname("Amit");
+        e1.setSalary(500000);
+        e1.setAddr(addr);
+        
         session.save(e1);
+        
         tr.commit();
         session.close();
-
+ 
+        
 	}
 
 }
